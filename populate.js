a55cf20a31778afb73ba705a4900ae11554eb1e3
JordonH03 <jordonhong03@gmail.com>
2023-11-24 22:19:55 -0600

    Add selected icon for grid songs

--- populate.js
+++ populate.js
@@ -22,17 +22,16 @@ function loadSongsInLibrary() {
     songList.forEach((song) => {
         const songBox = document.createElement("div");
         songBox.id = "grid-song-" + i;
-        i++;
         songBox.className =
-            "grid-song w-5/6 md:w-full p-2 rounded hover:cursor-pointer hover:bg-green-100";
+            "grid-song relative w-full p-2 rounded hover:cursor-pointer hover:bg-green-100";
 
         const songCover = document.createElement("img");
         songCover.src = song.image;
-        songCover.className = "w-full mb-4 rounded";
+        songCover.className = "hidden md:block w-full max-w-xs mb-4 rounded";
 
         //song info
         const songLabel = document.createElement("div");
-        songLabel.className = "w-11/12 whitespace-nowrap overflow-hidden";
+        songLabel.className = "whitespace-nowrap overflow-hidden";
 
         //title
         const title = document.createElement("p");
@@ -45,13 +44,21 @@ function loadSongsInLibrary() {
             "text-sm md:text-base lg:text-lg text-ellipsis overflow-hidden";
         artist.textContent = song.artist;
 
+        // selection position
+        const selectIcon = document.createElement("div");
+        selectIcon.id = "grid-song-" + i + "-selected";
+        selectIcon.className =
+            "hidden absolute -top-2 -right-2 lg:-top-3 lg:-right-3 w-8 h-8 lg:w-10 lg:h-10 flex items-center justify-center bg-yellow-300 rounded-full";
+
         songBox.appendChild(songCover);
 
         songLabel.appendChild(title);
         songLabel.appendChild(artist);
         songBox.appendChild(songLabel);
+        songBox.appendChild(selectIcon);
 
         songGrid.appendChild(songBox);
+        i++;
     });
 }
 
