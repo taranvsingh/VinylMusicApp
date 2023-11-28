b4428537a74e90510219933670b45438928ab616
JordonH03 <jordonhong03@gmail.com>
2023-11-27 22:10:03 -0600

    Add additional database songs

--- populate.js
+++ populate.js
@@ -1,25 +1,269 @@
 var songList = [
-    /* 
-    { title: str, artist: str, image: str}
-    */
-    { title: "Lucky Drive", artist: "Sarah Kinsley", image: "./assets/song-cover.jpg" },
+    {
+        title: "Cypress",
+        artist: "Sarah Kinsley",
+        image: "./assets/albums/cypress.jpg",
+    },
+    {
+        title: "Finish Line",
+        artist: "Delaney Bailey",
+        image: "./assets/albums/finish-line.jpg",
+    },
     {
         title: "Green",
         artist: "Sarah Kinsley",
-        image: "./assets/song-cover.jpg",
+        image: "./assets/albums/cypress.jpg",
     },
     {
-        title: "Hills of Fire",
+        title: "Lucky Drive",
         artist: "Sarah Kinsley",
-        image: "./assets/song-cover.jpg",
+        image: "./assets/albums/cypress.jpg",
+    },
+    {
+        title: "My Love Mine All Mine",
+        artist: "Mitski",
+        image: "./assets/songs/my-love.jpg",
+    },
+    {
+        title: "Sliver of Time",
+        artist: "Sarah Kinsley",
+        image: "./assets/albums/ascension.jpg",
+    },
+    {
+        title: "Starshps",
+        artist: "Nicki Minaj",
+        image: "./assets/songs/pink-friday.png",
     },
 ];
 
-function loadSongsInLibrary() {
+var playlistList = [
+    {
+        title: "Study Vibes",
+        creator: "Me",
+        image: "./assets/playlists/study-vibes.jpg",
+        songList: [
+            {
+                title: "Green",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/cypress.jpg",
+            },
+            {
+                title: "My Love Mine All Mine",
+                artist: "Mitski",
+                image: "./assets/songs/my-love.jpg",
+            },
+            {
+                title: "The Christmas Waltz",
+                artist: "Laufey",
+                image: "./assets/albums/cypress.jpg",
+            },
+        ],
+    },
+];
+
+var albumList = [
+    {
+        title: "Ascension",
+        artist: "Sarah Kinsley",
+        image: "./assets/albums/ascension.jpg",
+        songList: [
+            {
+                title: "Oh No Darling!",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/ascension.jpg",
+            },
+            {
+                title: "Black Horse",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/ascension.jpg",
+            },
+            {
+                title: "Ascension",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/ascension.jpg",
+            },
+            {
+                title: "Lovegod",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/ascension.jpg",
+            },
+            {
+                title: "Sliver of Time",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/ascension.jpg",
+            },
+        ],
+    },
+    {
+        title: "A Very Laufey Holiday!",
+        artist: "Laufey",
+        image: "./assets/albums/a-very-laufey-holiday.jpg",
+        songList: [
+            {
+                title: "Christmas Dreaming",
+                artist: "Laufey",
+                image: "./assets/albums/cypress.jpg",
+            },
+            {
+                title: "The Christmas Waltz",
+                artist: "Laufey",
+                image: "./assets/albums/cypress.jpg",
+            },
+            {
+                title: "Love to Keep Me Warm",
+                artist: "Laufey, dodie",
+                image: "./assets/albums/cypress.jpg",
+            },
+        ],
+    },
+    {
+        title: "Coffee Stained Smile",
+        artist: "Delaney Bailey",
+        image: "./assets/albums/coffee-stained-smile.jpg",
+        songList: [
+            {
+                title: "Coffee Stained Smile",
+                artist: "Delaney Bailey",
+                image: "./assets/albums/coffee-stained-smile.jpg",
+            },
+        ],
+    },
+    {
+        title: "Cypress",
+        artist: "Sarah Kinsley",
+        image: "./assets/albums/cypress.jpg",
+        songList: [
+            {
+                title: "Hills of Fire",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/cypress.jpg",
+            },
+            {
+                title: "What Was Mine",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/cypress.jpg",
+            },
+            {
+                title: "Green",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/cypress.jpg",
+            },
+            {
+                title: "Cypress",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/cypress.jpg",
+            },
+            {
+                title: "Lucky Drive",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/cypress.jpg",
+            },
+        ],
+    },
+    {
+        title: "Finish Line",
+        artist: "Delaney Bailey",
+        image: "./assets/albums/finish-line.jpg",
+        songList: [
+            {
+                title: "Finish Line",
+                artist: "Delaney Bailey",
+                image: "./assets/albums/finish-line.jpg",
+            },
+        ],
+    },
+    {
+        title: "Perfect Night",
+        artist: "LE SSERAFIM",
+        image: "./assets/albums/perfect-night.jpg",
+        songList: [
+            {
+                title: "Perfect Night",
+                artist: "LE SSERAFIM",
+                image: "./assets/albums/perfect-night.jpg",
+            },
+        ],
+    },
+];
+
+var artistList = [
+    {
+        name: "Sarah Kinsley",
+        image: "./assets/artists/sarah-kinsley.jpg",
+        discography: [
+            {
+                title: "Ascension",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/ascension.jpg",
+                songList: [
+                    {
+                        title: "Oh No Darling!",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/ascension.jpg",
+                    },
+                    {
+                        title: "Black Horse",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/ascension.jpg",
+                    },
+                    {
+                        title: "Ascension",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/ascension.jpg",
+                    },
+                    {
+                        title: "Lovegod",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/ascension.jpg",
+                    },
+                    {
+                        title: "Sliver of Time",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/ascension.jpg",
+                    },
+                ],
+            },
+            {
+                title: "Cypress",
+                artist: "Sarah Kinsley",
+                image: "./assets/albums/cypress.jpg",
+                songList: [
+                    {
+                        title: "Hills of Fire",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/cypress.jpg",
+                    },
+                    {
+                        title: "What Was Mine",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/cypress.jpg",
+                    },
+                    {
+                        title: "Green",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/cypress.jpg",
+                    },
+                    {
+                        title: "Cypress",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/cypress.jpg",
+                    },
+                    {
+                        title: "Lucky Drive",
+                        artist: "Sarah Kinsley",
+                        image: "./assets/albums/cypress.jpg",
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+function loadSongs(list) {
     const songGrid = document.getElementById("song-grid");
 
     i = 0;
-    songList.forEach((song) => {
+    list.forEach((song) => {
         const songBox = document.createElement("div");
         songBox.id = "grid-song-" + i;
         songBox.className =
@@ -61,5 +305,3 @@ function loadSongsInLibrary() {
         i++;
     });
 }
-
-loadSongsInLibrary();
